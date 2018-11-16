55a12c0bbcaaae70e6f539202863fcf00814192a
Liubov Stepanova <34545151+Step-Li@users.noreply.github.com>
2018-11-16 12:59:23 +0300

    Add files via upload

--- src/app/api/in-memory-data.service.ts
+++ src/app/api/in-memory-data.service.ts
@@ -9,13 +9,13 @@ export class InMemoryDataService implements InMemoryDbService {
 	createDb() {
 		const indicators: Indicator[] = [];
 		for (let i = 1; i <= 8; i++) {
-    		let min = Math.abs(Math.random()) * 500 + 500; 
+    		let min = Math.random() * 500 + 500; 
      		indicators.push({
 	        	id: `${i}`,
 	            title: `Уровень карналита в силосной башне №${i}`,
-	            value: Math.round(Math.abs(Math.random()) * 1500 + 500),
+	            value: Math.round(Math.random() * 1500 + 500),
 	            minValue: Math.round(min),
-	            maxValue: Math.round(min + Math.abs(Math.random()) * 1000 + 500)
+	            maxValue: Math.round(min + Math.random()) * 1000 + 500
 	        });
     	}
 		return { indicators };
